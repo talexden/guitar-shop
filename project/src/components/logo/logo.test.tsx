9c24e4e952ebb0f53558fb869313a335cc24ed21
alex <talexden@gmail.com>
2022-02-01 00:28:06 +0300

    добавляет тест для компонента 'Logo'

---
+++ project/src/components/logo/logo.test.tsx
@@ -0,0 +1,43 @@
+import {createMemoryHistory} from 'history';
+import {render, screen} from '@testing-library/react';
+import {Route, Router, Switch} from 'react-router-dom';
+import Logo from './logo';
+// import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+
+
+const history = createMemoryHistory();
+describe('Component: Logo', () => {
+  it('should render correctly', () => {
+    const styleClassName = 'logo__fake';
+
+    render(
+      <Router history={history}>
+        <Logo className={styleClassName}/>
+      </Router>);
+
+    expect(screen.getByAltText(/Логотип/i)).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+  });
+
+  it('should redirect to root url when user clicked to link', () => {
+    history.push('/fake');
+    const styleClassName = 'logo__fake';
+
+    render(
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact>
+            <h1>This is main page</h1>
+          </Route>
+          <Route>
+            <Logo className={styleClassName}/>
+          </Route>
+        </Switch>
+      </Router>);
+
+    expect(screen.queryByText(/This is main page/i)).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole('link'));
+    expect(screen.queryByText(/This is main page/i)).toBeInTheDocument();
+  });
+});
